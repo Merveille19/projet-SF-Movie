e068f9c2415f48536a3355b48e56eb17bb7552cd
Merveille19 <144835423+Merveille19@users.noreply.github.com>
2025-02-28 10:22:47 +0100

    index
    update3

--- map.js
+++ map.js
@@ -1,3 +1,4 @@
+
 var map = L.map('map').setView([51.505, -0.09], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -7,3 +8,8 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 L.marker([51.5, -0.09]).addTo(map)
     .bindPopup('A pretty CSS popup.<br> Easily customizable.')
     .openPopup();
+
+    // creons une fonction our recuperer la longitude et latitude de nos objets dans le fichier json
+    function searchtomap(data){
+        
+    }
