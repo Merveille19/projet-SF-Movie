585f1502f5cf062999592f92bafeb7db1bc1b130
Merveille19 <144835423+Merveille19@users.noreply.github.com>
2025-03-02 20:56:08 +0100

    index
    udapte7

--- map.js
+++ map.js
@@ -1,5 +1,7 @@
 const markers = [] ;
 const movies = [] ;
+let map;
+
 // Interaction avec l'API pour récupérer des données externes 
 fetch("./data.json") // utilisation de l'API fetch our charger notre fichier
 .then((response) => response.json()) // convertit la reponse en json 
@@ -11,7 +13,7 @@ fetch("./data.json") // utilisation de l'API fetch our charger notre fichier
 .catch((error) => console.log("shit happened ", error)) 
 
 
-let map = L.map('map').setView([37.773972, -122.431297], 13); // 13 reresente la dimension
+map = L.map('map').setView([37.773972, -122.431297], 13); // 13 reresente la dimension
     
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -22,32 +24,34 @@ L.marker([37.773972, -122.431297]).addTo(map)
     .openPopup();
        
 // effacer les marqueurs existant
-    function addMarkersToMap(movies, markers){      
+    function addMarkersToMap(movies){      
     markers.forEach(marker => map.removeLayer(marker));
-    //markers = []
+    markers = [];
 
     // recherchons les nouveaux marqueurs
         movies.forEach(movie =>{
         if (movie.lng && movie.lat){
             const longitude = parseFloat( movie.lng);
             const latitude = parseFloat(movie.lat);
-            const marker = L.marker([longitude, latitude]).addTo(map)
-            .bindPopup(`
+            const marker = L.marker([longitude, latitude])
+                .addTo(map)
+                .bindPopup(`
                 longitude : ${movie.lng}
                 latitude : ${movie.lat}`);
             
-           markers.push(marker);     
+            markers.push(marker);     
         }
         });
 
     }
     
+    // recherche et filtrage
 let titrefilm = document.getElementsByClassName('searchbar').addEventListener('input', searchfilter())
 
 function searchfilter(movies){
     let searchresult =  titrefilm.value.toLowerCase(); // convertissonms le texte de entre en miniscule
     let searchresultf = movies.filter(movie => movie.title.toLowerCase().includes(searchresult));
-     
+     addMarkersToMap(searchresultf)
 }
             
 
